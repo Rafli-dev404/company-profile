0564f312735cfe0a87af0b11f6023879c08dd0aa
Rafli-dev404 <raflirohimat404@gmail.com>
2025-09-08 11:19:58 +0700

    required

--- resources/js/Pages/Login/Components/LoginForm.tsx
+++ resources/js/Pages/Login/Components/LoginForm.tsx
@@ -43,7 +43,6 @@ export default function LoginForm() {
                     onChange={(e) => setData("password", e.target.value)}
                     className="w-full mt-1 border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
                     placeholder="Enter your password"
-                    required
                 />
                 {errors.password && (
                     <p className="text-red-500 text-xs mt-1">{errors.password}</p>
